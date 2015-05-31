817c5d01b4ff6407c7d447095a4f327f40b5bc64
Tomasz Rzepka <tomaszrzepka@hotmail.com>
2015-05-31 15:16:51 +0200

    fix

--- __main__.py
+++ __main__.py
@@ -11,22 +11,22 @@ if __name__ == "__main__":
 
     def _creator():
         screen2 = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
-        gr = Menu.NewGame(screen2)
+        gr = menu.NewGame(screen2)
         gr.run()
 
     def _credits():
         screen3 = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
-        cr = Menu.Credits(screen3)
+        cr = menu.Credits(screen3)
         cr.run()
 
     def _settings():
         screen4 = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
-        st = Menu.Settings(screen4)
+        st = menu.Settings(screen4)
         st.run()
 
-	config.load()
+    config.load()
     SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
     FUNCS = (("New Game", _creator), ("About", _credits), ("Settings", _settings), ("Exit", sys.exit))
     pygame.display.set_caption("PyTank")
-    GM = Menu.GameMenu(SCREEN, FUNCS)
+    GM = menu.GameMenu(SCREEN, FUNCS)
     GM.run()
--- menu/__init__.py
+++ menu/__init__.py
@@ -4,3 +4,4 @@ from credits import Credits
 from new_game import NewGame
 from game_menu import GameMenu
 from configs import *
+from settings import Settings
